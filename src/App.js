b0ec78aa727e04de22b1ab5088130538c6aadec4
gunesh <gunesh26@gmail.com>
2022-04-23 18:19:03 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -3,9 +3,21 @@ import './style.css';
 import Recursion from './recursion';
 import files from '../data/test.json';
 
+const Exmple = ()=>{
+  let obj = new Promise((resolve,reject)=>{
+    if(1===1){
+      resolve("Success")
+    }else{
+      reject("Failed")
+    }
+  })
+  console.log(obj)
+ return <div>Ok</div>
+}
 export default function App() {
   return (
     <div>
+      <Exmple />
       <Recursion files={files} />
     </div>
   );
