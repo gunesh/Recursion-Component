7b6a2a8350843d4633e9871c783c2946c3a42801
gunesh <gunesh26@gmail.com>
2022-04-23 21:00:36 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './style.css';
 import Recursion from './recursion';
 import files from '../data/test.json';
 
-const Exmple = ()=>{
-  let obj = new Promise((resolve,reject)=>{
-    if(1===1){
-      resolve("Success")
-    }else{
-      reject("Failed")
-    }
-  })
-  console.log(obj)
- return <div>Ok</div>
-}
+// function Exmple(){
+//   return new Promise(function (resolve, reject) {
+//     setTimeout(function () {
+//       resolve('Success!');
+//     }, 2000);
+//   });
+// };
 export default function App() {
+  const [val, setVal] = useState('Initial State');
+
+  useEffect(() => {
+    console.log('Hello');
+    var obj = new Promise(function (resolve, reject) {
+      // setTimeout(function () {
+      //   resolve('Success!');
+      // }, 2000);
+      if (1 === 1) {
+        resolve('Success!');
+      } else {
+        reject('Failed!');
+      }
+    });
+    obj.then(
+      (data) => {
+        setVal(data);
+      },
+      (err) => {
+        setVal(err);
+      }
+    );
+  });
+
   return (
     <div>
-      <Exmple />
+      {val}
       <Recursion key={'a'} files={files} />
     </div>
   );
